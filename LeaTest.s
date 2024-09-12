a93b3b4bec82bbfed8f89b200ec0508509358273
VirtualWizard100 <139145827+VirtualWizard100@users.noreply.github.com>
2024-09-12 12:17:42 -0400

    Update LeaTest.s

--- LeaTest.s
+++ LeaTest.s
@@ -10,9 +10,9 @@ Out: db 0
 section .text
 _start:
 	mov r10, message
-	lea r9, [message+1]
-	mov r10, [r9]
-	mov [Out], r10b
+	lea r9, [message+1] 	;r9 obtains the address at the 1 byte offset from message (or the start of db in message, they both have the same address)
+	mov r10, [r9]		;Loading the value stored at the address that r9 is pointing to into r10
+	mov [Out], r10b		;storing r10's new value into the empty byte in Out
 	mov al, 1
 	mov dil, 1
 	mov rsi, Out
