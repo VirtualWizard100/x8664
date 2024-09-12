11e6f909955de60f47355f8f5f7f236311c656fb
VirtualWizard100 <139145827+VirtualWizard100@users.noreply.github.com>
2024-09-12 17:45:50 -0400

    Add files via upload

--- fileio.s
+++ fileio.s
@@ -4,28 +4,28 @@ STDOUT equ 1
 STDERR equ 2
 
 ;chmod
-O_READ equ 0004
-O_WRITE equ 0002
-O_EXEC equ 0001
-G_READ equ 0040
-G_WRITE equ 0020
-G_EXEC equ 0010
-U_READ equ 0400
-U_WRITE equ 0200
-U_EXEC equ 0100
-O_RW equ 0006
-O_RX equ 0005
-O_WX equ 0003
-O_RWX equ 0007
-G_RW equ 0060
-G_RX equ 0050
-G_WX equ 0030
-G_RWX equ 0070
-U_RW equ 0600
-U_RX equ 0500
-U_WX equ 0300
-U_RWX equ 0700
-UGO_RWX equ 0777
+O_READ equ 0o004
+O_WRITE equ 0o002
+O_EXEC equ 0o001
+G_READ equ 0o040
+G_WRITE equ 0o020
+G_EXEC equ 0o010
+U_READ equ 0o400
+U_WRITE equ 0o200
+U_EXEC equ 0o100
+O_RW equ 0o006
+O_RX equ 0o005
+O_WX equ 0o003
+O_RWX equ 0o007
+G_RW equ 0o060
+G_RX equ 0o050
+G_WX equ 0o030
+G_RWX equ 0o070
+U_RW equ 0o600
+U_RX equ 0o500
+U_WX equ 0o300
+U_RWX equ 0o700
+UGO_RWX equ 0o777
 
 %macro read 3
 	mov al, 0
@@ -44,8 +44,8 @@ UGO_RWX equ 0777
 %endmacro
 
 %macro chmod 2
-	mov ax, 60
+	mov eax, 0x5a
 	mov rdi, %1
-	mov esi, %2
+	mov rsi, %2
 	syscall
 %endmacro
