4efd9be9cdcc87d26287bf56106dee0c7f92a2cc
VirtualWizard100 <139145827+VirtualWizard100@users.noreply.github.com>
2024-12-09 18:58:50 -0500

    Add files via upload
    An x86-64 Assembly program that remotely disconnects a usb on your device of your choosing with ioctl.

---
+++ usbDisconnectTest.s
@@ -0,0 +1,26 @@
+%include "fileio.s"
+global _start
+
+section .text
+_start:
+	openat -100, file, UGO_RW, STDIN
+	mov edi, eax
+	jmp Ioctl
+Ioctl:
+	mov eax, 0x10
+	mov esi, 0xc0105512
+	lea rdx, [USBDEVFS_DISCONNECT]
+	syscall
+	jmp exit
+
+exit:
+	mov eax, 0x3c
+	xor edi, edi
+	syscall
+
+section .data
+USBDEVFS_DISCONNECT:
+	dd 0x1
+	dd 0x00005516
+file:
+	db "/dev/bus/usb/001/004", 0
