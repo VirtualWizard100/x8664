967b81fece18f2a76f61536049f403ba42045ce6
VirtualWizard100 <139145827+VirtualWizard100@users.noreply.github.com>
2026-04-08 11:54:07 -0400

    Add files via upload
    Deleted the ret instruction at the end of %%sortSecond, there's nothing to ret to, and cleared the registers at the end of the macro since they don't automatically get cleared.

--- hextoasciimacro.s
+++ hextoasciimacro.s
@@ -9,7 +9,11 @@
 
 %%valueSort:
 	cmp r10, r11
+%ifdef OUTPUT
         je %%write
+%else
+	je %%end
+%endif
 	mov r12b, BYTE [%1 + r13]
 	inc r13
 
@@ -18,25 +22,24 @@
 	shr r9b, 0x4
 	cmp r9b, 0xa
 	jl %%number_high
-	add r9, (0x41 - 0xa) ;add Ascii A - 0xa (10)
+	add r9, (0x41 - 0xa) ; add Ascii A - 0xa (10)
 	jmp %%store_high
 
 %%number_high:
-	add r9, 0x30
+	add r9, 0x30	; add Ascii 0
 
 %%store_high:
 	mov BYTE [%3 + r10], r9b
 	inc r10
 	jmp %%sortsecond
 
-%%sortsecond:
+%%sortsecond:		; for the second nibble
         mov r9b, r12b
         and r9b, 0xf
         cmp r9b, 0xa
         jl %%number_low
-        add r9, (0x41 - 0xa) ;add Ascii A - 0xa (10)
+        add r9, (0x41 - 0xa) ; add Ascii A - 0xa (10)
         jmp %%store_low
-        ret
 
 %%number_low:
 	add r9b, 0x30
@@ -45,12 +48,21 @@
 	mov BYTE [%3 + r10], r9b
 	inc r10
 	jmp %%valueSort
-
+%ifdef OUTPUT
 %%write:
         mov eax, 0x1
         mov rdi, 0x1
         mov rsi, %3
         mov rdx, r11
         syscall
+%else
+%%end:
+%endif
+	xor r8, r8
+	xor r9, r9
+	xor r10, r10
+	xor r11, r11
+	xor r12, r12
+	xor r13, r13
 %endmacro
 
